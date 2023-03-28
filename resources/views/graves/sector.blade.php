773fdc7ff6d715c2066019f549a774b66c9abdca
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2023-03-28 20:21:28 +0800

    update

--- resources/views/graves/sector.blade.php
+++ resources/views/graves/sector.blade.php
@@ -5,7 +5,7 @@
         <h1 class="my-5">{{ $sector->name }} Graves</h1>
         @foreach($graves as $grave)
             @if($grave->person)
-                <a href="{{ route('persons.show', $grave->person) }}" class="p-5 btn btn-sm btn-primary mx-1 my-1" data-toggle="tooltip" data-placement="top" title="{{ $grave->person->full_name }}">
+            <a href="{{ route('persons.show', ['person' => $grave->person, 'grave_number' => $loop->iteration]) }}" class="p-5 btn btn-sm btn-primary mx-1 my-1" data-toggle="tooltip" data-placement="top" title="{{ $grave->person->full_name }}">
                     {{ $loop->iteration }}
                     <!-- {{ $grave->id }} -->
                 </a>
